c8ea01eac534d4b6b2d64639fa9d013785aa4c82
nimishlal <56611481+nimishlal@users.noreply.github.com>
2021-08-04 11:13:07 -0700

    Add inputs for email and password

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,6 +18,34 @@ export default function Login() {
     )
 
     return (
-        <div className="container flex mx-auto max-w-screen-md items-center h-screen"> <p>This is the login portion</p> </div>
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+                </h1>
+                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+                <form onSubmit={handleLgoin} method="Post">
+                    <input aria-label="Enter your email address"
+                        type="text" placeholder="Email address" 
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange= { ({target }) => setEmailAddress(target.value)}
+                        > 
+                    </input>
+                    <input aria-label="Enter your password"
+                        type="text" placeholder="Password" 
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange= { ({target }) => setPassword(target.value)}
+                        > 
+                    </input>
+                </form>
+            </div>
+        </div>
     );
 }
+
+// tailwind config items
+// text-red-primary
